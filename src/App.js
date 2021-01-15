0dfd49130d93f4802ed2d349f952a9cbc38ae0c2
Amit Agrawal <amit.agrawal@hyperverge.co>
2021-01-15 12:21:55 +0530

    init

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>UNDER CONSTRUCTION</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+        Site almost ready. Please visit again in some time.
+        </p>
       </header>
     </div>
   );
